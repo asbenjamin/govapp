dd0f24d5a5ede003bb2b19a901b85353fd17cff3
asbenjamin <shammahbenjamin@gmail.com>
2022-01-25 22:45:58 +0300

    a few more changes

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 
@@ -13,12 +14,20 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        #this new return returns qns whose pub date is earlier than or equal to timezone.now
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')[:5]
+        #return Question.objects.order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):   #how does django know which fields of this model to display in the template
     model = Question                    #I think this is controlled in the html template, using tags and django code
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
